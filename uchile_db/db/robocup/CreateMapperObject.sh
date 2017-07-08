2b2a3b5fab6ec3520ad76d3d6e84db16a8c71b44
benderuchile <bender.devel@gmail.com>
2017-07-07 23:57:07 -0400

    scripts robocup

---
+++ uchile_db/db/robocup/CreateMapperObject.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#ejemplo : ./CreateMapperObject.sh GermanOpen2017/Objects/
+
+dirlist=(${1}*)
+obj='<small_objects> = '
+objmaqui='<small_objects> = '
+for dir in "${dirlist[@]}"; do 
+	echo "$dir"; 
+	onlyname="${dir/$1/}"
+	onlyname="${onlyname/.jpg/}"
+	nameobj=${onlyname%-*}  
+	categobj=${onlyname#*-} 
+	nameobj=`echo "$nameobj" | tr '[:upper:]' '[:lower:]' | sed 's/[[:space:]]*$//'`
+	nameCamel=`echo "$nameobj" | tr '[:lower:]' '[:upper:]'`
+	categobj=`echo "$categobj" | tr '[:upper:]' '[:lower:]' | sed 's/[[:space:]]*$//' | sed 's/^[[:space:]]*//'`
+
+	nameobj2="${nameobj/ /_}"
+	nameCamel2="${nameCamel/ /_}"
+	categobj2="${categobj/ /_}"
+
+	if echo "$nameobj" | grep -q " "; then
+		echo "$nameobj: $nameobj2" >> ParserNames.yaml
+	fi
+	if echo "$categobj" | grep -q " "; then
+		echo "$categobj: $categobj2" >> ParserNames.yaml
+	fi
+	nameobj2="$nameobj"
+	nameCamel2="$nameCamel"
+	categobj2="$categobj"
+
+	if echo "$nameobj" | grep -q ","; then
+	  echo "OJO!! hay que separarlos";
+	  echo "$nameobj: ['$categobj', 0.15, 0.15]" >> ObjectInformation.yaml
+	else
+	  echo "$nameobj: ['$categobj', 0.15, 0.15]" >> ObjectInformation.yaml
+	fi
+	obj=$obj$nameCamel" | "
+	objmaqui=$objmaqui$nameobj" | "
+done
+obj=$obj";"
+objmaqui=$objmaqui";"
+
+echo $obj >> diccionary.txt
+echo $objmaqui >> diccionary.txt
