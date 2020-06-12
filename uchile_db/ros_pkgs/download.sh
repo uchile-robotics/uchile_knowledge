5efab56a6a5f65d6c306ef72280a2aacb6d98ecd
lpavezb <lukas.pavez@ug.uchile.cl>
2020-06-11 23:48:10 -0400

    change links from mega to google drive

--- uchile_db/ros_pkgs/download.sh
+++ uchile_db/ros_pkgs/download.sh
@@ -5,10 +5,10 @@
 #!/bin/bash
 
 # Download bender_arm_planning
-"$UCHILE_SYSTEM"/shell/megadown/megadown 'https://mega.nz/#!ik0XUKKI!kzyZp2hbg09Ol_Tg1MuFlqxIfR8F01h4HGOKl3lHpos'
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1hZqcysm9BIaoonL8uy8_UptMmxdg03Qa' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1hZqcysm9BIaoonL8uy8_UptMmxdg03Qa" -O bender_arm_planning.tar.gz && rm -rf /tmp/cookies.txt
 
 # Download bender_description
-"$UCHILE_SYSTEM"/shell/megadown/megadown 'https://mega.nz/#!a0930ZYB!sciM89vyDLtrpDpC5_CB-6BCXA6OE1p2Bzq8R6rWeRU'
+wget --no-check-certificate -nd 'https://docs.google.com/uc?export=download&id=1yV7pFwRLPtkPnKqR5rkvcYgd4Iabi9f-' -O bender_description.tar.gz
 
 # Download uchile_fun
-"$UCHILE_SYSTEM"/shell/megadown/megadown 'https://mega.nz/#!K1F3jRgY!S20epp5617NV7BkEaSaRCblGgZ9MoT2M9n1f_pP6Gck'
+wget --no-check-certificate -nd 'https://docs.google.com/uc?export=download&id=1bW1yWd2icW5kl3m6vYYsfd2mvpommNS7' -O uchile_fun.tar.gz
